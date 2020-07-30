538d108c9ebaff33bd5b2d4ff993b7930e0ddaae
Zach Calvert <zcalvert@oreilly.com>
2020-07-29 21:29:29 -0700

    Style only cards, not other svgs

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,20 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { ReactSVG } from 'react-svg'
 import { makeStyles, Divider } from "@material-ui/core";
-
-const styles = makeStyles((theme) => ({
-  playingCard: {
-    height : 160,
-    borderRadius: '5px'
-  },
-  ' svg': {
-    height: 160,
-    width: 'auto'
-  },
-  ' rect': {
-    height: 'auto'
-  }
-}));
+import './Cards.css'
 
 export const Player = (name) => {
 
@@ -22,13 +9,13 @@ export const Player = (name) => {
     <div className="player">
       <div className="player-name">{ name.name }</div>
       <Divider variant="middle" />
-      <span className={styles.cardRow}>
-        <ReactSVG wrapper='span' src="cards/2H.svg" { ...styles } />
-        <ReactSVG wrapper='span' src="cards/AS.svg" { ...styles } />
-        <ReactSVG wrapper='span' src="cards/JD.svg" { ...styles } />
-        <ReactSVG wrapper='span' src="cards/JD.svg" { ...styles } />
-        <ReactSVG wrapper='span' src="cards/JD.svg" { ...styles } />
-        <ReactSVG wrapper='span' src="cards/JD.svg" { ...styles } />
+      <span>
+        <ReactSVG wrapper='span' src="cards/2H.svg" />
+        <ReactSVG wrapper='span' src="cards/AS.svg" />
+        <ReactSVG wrapper='span' src="cards/JD.svg" />
+        <ReactSVG wrapper='span' src="cards/JD.svg" />
+        <ReactSVG wrapper='span' src="cards/JD.svg" />
+        <ReactSVG wrapper='span' src="cards/JD.svg" />
       </span>
     </div>
   );
