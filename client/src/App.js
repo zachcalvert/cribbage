4b200828b3d3e40a59fd3bff2812b7eb34bd0d36
Zach Calvert <zcalvert@oreilly.com>
2020-08-25 11:19:54 -0700

    Expose flask via NodePort service, set backend service IP in Dockerfile

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { SocketIOProvider } from "use-socketio";
 import { Game } from "./components/Game";
 import './App.css';
 
-
 export const App = () => (
   <SocketIOProvider url={process.env.REACT_APP_SOCKET_HOST}>
     <Game />
