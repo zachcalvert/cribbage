45c19bb31d0aa0f2a03aecbf0c270947ca6a6b81
Zach Calvert <zcalvert@oreilly.com>
2021-01-10 09:53:06 -0800

    display hearts and diamonds as red in chat

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -58,7 +58,7 @@ class CribbageNamespace(Namespace):
         player = game["previous_turn"]["player"]
         hand = 'crib' if 'crib' in game['previous_turn']['action'] else 'hand'
         short_hand = ' '.join([utils.card_short_text_from_id(card_id) for card_id in game['hands'][player] + [game['cut_card']]])
-        message = f'Scoring {game["previous_turn"]["player"]}\'s {hand} ({short_hand})'
+        message = f'Scoring {game["previous_turn"]["player"]}\'s {hand}: {short_hand}'
         time.sleep(1)
         self.announce(message, room=game['name'])
 
