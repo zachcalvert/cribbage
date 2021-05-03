e2a768f7f1a191b9e40c5cd6ea1f5e92345beca0
Zach Calvert <zcalvert@oreilly.com>
2021-05-02 21:23:56 -0400

    add shebang

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,6 +1,8 @@
-until cd /app/backend/server
+#!/bin/sh
+
+until cd /app/backend
 do
     echo "Waiting for server volume..."
 done
 
-gunicorn -b 0.0.0.0:5000 --access-logfile --worker-class eventlet -w 1 app:app
+gunicorn -b 0.0.0.0:5000 --worker-class eventlet -w 1 app:app
