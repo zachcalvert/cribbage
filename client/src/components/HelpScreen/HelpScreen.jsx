4606617ea236c5f929a7868998b714f1b8e38d38
Zach Calvert <zcalvert@oreilly.com>
2021-06-14 15:56:02 -0700

    sadhfasdh

--- client/src/components/HelpScreen/HelpScreen.jsx
+++ client/src/components/HelpScreen/HelpScreen.jsx
@@ -10,6 +10,7 @@ export const HelpScreen = () => {
         <Typography variant='subtitle2'>
           <br />
           <ul style={{"text-align": "left"}} >
+            <li>This is a new version of the game, I appreciate your patience as I get everything working :)</li>
             <li>If the game ever appears to be stuck, please try refreshing the page.</li>
             <li>If you ever notice anything broken, or have ideas on how to make the game more fun, please email me at zach@cribbage.live</li>
           </ul>
