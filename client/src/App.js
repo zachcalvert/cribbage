9e37b7ac67adf8e23899030a2bb5af4b0f444b6a
Zach Calvert <zacharym.calvert@gmail.com>
2024-04-08 08:40:37 -0700

    working socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React  from 'react';
+import React, { useEffect, useState}  from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { green, orange } from '@mui/material/colors';
 
+import { socket } from './socket';
 import Header from './components/Header/Header';
 import Game from "./components/Game";
 
@@ -16,9 +17,37 @@ const outerTheme = createTheme({
   },
 });
 
-export const App = () => (
-  <ThemeProvider theme={outerTheme}>
-    <Header />
-    <Game />
-  </ThemeProvider>
-);
+export default function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+      console.log('connected!')
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
+  return (
+    <ThemeProvider theme={outerTheme}>
+      <Header isConnected={isConnected} />
+      <Game />
+    </ThemeProvider>
+  )
+};
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,16 +7,13 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import StyleIcon from '@mui/icons-material/SportsBar';
 
-const pages = ['Football', 'Content'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function Header() {
+function Header({ isConnected }) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -110,33 +107,10 @@ function Header() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+          {isConnected ?
+                <CheckCircleIcon color="secondary" />
+                : <CheckCircleIcon color="error" />
+            }
           </Box>
         </Toolbar>
       </Container>
