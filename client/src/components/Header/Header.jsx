05a9020646fed9d4f32962ac06b9ac7de1b9cda5
Zach Calvert <zacharym.calvert@gmail.com>
2024-04-08 16:13:15 -0700

    remove unnecessary header code

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,36 +2,14 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import StyleIcon from '@mui/icons-material/SportsBar';
+import StyleIcon from '@mui/icons-material/Style';
 import CancelIcon from '@mui/icons-material/Cancel';
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 function Header({ isConnected }) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar position="static" enableColorOnDark>
@@ -56,36 +34,6 @@ function Header({ isConnected }) {
             Cribbage
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-          />
-          </Box>
           <Typography
             variant="h5"
             noWrap
